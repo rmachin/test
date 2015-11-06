c7a5381f21ddebdd7949f97e054a47ba79e26f16
REMACHIN.CON SL3257398 <info@remachin.com>
2015-11-05 19:11:03 -0500

    Initial Commit

---
+++ Prototipo X/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prototipo X
+//
+//  Created by Raphael E Machin on 11/5/15.
+//  Copyright © 2015 Raphael Machin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
